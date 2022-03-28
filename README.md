6e6c9a6f15ad95f076a001d3f072bded44f646e9
wbevans <wbevans@nullified.net>
2022-03-28 11:00:39 -0500

    update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # archinstall-config
+
+bash <(curl -sL https://raw.githubusercontent.com/wbevans/archinstall-config/main/run.sh)
--- run.sh
+++ run.sh
@@ -1,1 +1,11 @@
+pacman -Sy --noconfirm; pacman -S --noconfirm git python-pip
+pip uninstall --yes archinstall
+git clone https://github.com/archlinux/archinstall .archinstall
+cd .archinstall
+python setup.py install
+
+cd
+git clone https://github.com/wbevans/archinstall-config.git
+cd archinstall-config
+
 archinstall --config config.json --creds creds.json --disk_layouts=disk-layouts.json
