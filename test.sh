82a13ac9ff784033036a458e91c7b5f6aebe4a18
wbevans <wbevans@nullified.net>
2022-03-28 10:52:19 -0500

    tst

---
+++ test.sh
@@ -0,0 +1,5 @@
+curl -O https://raw.githubusercontent.com/wbevans/archinstall-config/main/config.json
+curl -O https://raw.githubusercontent.com/wbevans/archinstall-config/main/creds.json
+curl -O https://raw.githubusercontent.com/wbevans/archinstall-config/main/disk-layouts.json
+curl -O https://raw.githubusercontent.com/wbevans/archinstall-config/main/run.sh 
+curl -O https://raw.githubusercontent.com/wbevans/archinstall-config/main/archinstall_latest.sh
